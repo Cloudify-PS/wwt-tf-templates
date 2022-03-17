c25c766a7eda451fa35ef58269e17512514e47a9
Michał Mordawski <michal.mordawski@amartus.com>
2022-03-17 12:19:43 +0100

    runner installation with commands

--- scripts/configure-runner.sh
+++ scripts/configure-runner.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 
 cd "/home/labuser/${APP_NAME}-runner"
+./svc.sh install
 ./svc.sh start
--- scripts/install-runner.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir "${APP_NAME}-runner" && cd "${APP_NAME}-runner"
-curl -o actions-runner-linux-x64-2.288.1.tar.gz \
-  -L https://github.com/actions/runner/releases/download/v2.288.1/actions-runner-linux-x64-2.288.1.tar.gz
-tar xzf ./actions-runner-linux-x64-2.288.1.tar.gz
-./config.sh --url https://github.com/${REPO_NAME} --token ${REGISTRATION_TOKEN}
